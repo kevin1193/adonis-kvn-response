c43cc73b328070ce0a1ceddc7269b77d909828b3
Kevin Abestilla <kev.abestilla@gmail.com>
2021-12-10 16:18:59 +0800

    Update new response

--- src/providers/KvnResponseProvider.ts
+++ src/providers/KvnResponseProvider.ts
@@ -1,40 +1,114 @@
 
-import KvnResponse from '../KvnResponse'
 import { ApplicationContract } from '@ioc:Adonis/Core/Application'
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import TransformerAbstractI from 'adonis-bumblebee-ts/build/Bumblebee/TransformerAbstract'
 
-declare module '@ioc:Adonis/Core/HttpContext' {
-  interface HttpContextContract {
-    kvnResponse?: any
+declare module '@ioc:Adonis/Core/Response' {
+  interface ResponseContract {
+    ok(message: String): this
+
+    data(data: {}, message: String): this
+
+    resource(resource: TransformerAbstractI, meta:{}, message: String): this
+
+    accepted(data: {}, message: String): this
+
+    error(errors: {}, message: String): this
+
+    unableToProcess(errors: {}, message: String): this
+
+    notFound(message: String): this
+
+    forbidden(message: String): this
+
+    unauthorized(message: String): this
+
+    badRequest(message: String): this
   }
 }
 
 export default class AppProvider {
   public static needsApplication = true
 
-  constructor (protected app: ApplicationContract, protected context: HttpContextContract) {}
-
-  public register () {
-    const kvnResponse = new KvnResponse(this.context)
-
-    this.app.container.singleton('Adonis/Addons/KvnResponse', () => kvnResponse)
-  }
+  constructor (protected application: ApplicationContract) {}
 
   public async boot () {
-    const Context = this.app.container.use('Adonis/Core/HttpContext')
-    Context.getter(
-      'kvnResponse',
-      () => {
-        return new KvnResponse(this.context)
-      }
-    )
-  }
+    const Response = this.application.container.use('Adonis/Core/Response')
 
-  public async ready () {
-    // App is ready
-  }
+    Response.macro('ok', function (message: String) {
+      this.status(200).json({
+        message,
+      })
+      return this
+    })
 
-  public async shutdown () {
-    // Cleanup, since app is going down
+    Response.macro('data', function (data:{}, message: String = 'Ok') {
+      this.status(200).json({
+        message,
+        data,
+      })
+      return this
+    })
+
+    Response.macro('resource', function (resource:TransformerAbstractI, meta:{} = {}, message:String = 'Ok') {
+      const data = resource
+      this.status(200).json({
+        message,
+        data,
+        meta,
+      })
+      return this
+    })
+
+    Response.macro('accepted', function (data: {}, message: String= 'Accepted') {
+      this.status(202).json({
+        message,
+        data,
+      })
+      return this
+    })
+
+    Response.macro('error', function (errors: {}, message: String= 'Internal Server Error') {
+      this.status(500).json({
+        message,
+        errors,
+      })
+      return this
+    })
+
+    Response.macro('unableToProcess', function (errors:{}, message:String = 'Unprocessable Entity') {
+      this.status(422).json({
+        message,
+        errors,
+      })
+      return this
+    })
+
+    Response.macro('notFound', function (message:String = 'Not Found') {
+      this.status(404).json({
+        message,
+      })
+      return this
+    })
+
+    Response.macro('forbidden', function (message:String = 'Forbidden') {
+      this.status(403).json({
+        message,
+      })
+      return this
+    })
+
+    Response.macro('unauthorized', function (message:String = 'Unathorized') {
+      this.status(401).json({
+        message,
+      })
+      return this
+    })
+
+    Response.macro('badRequest', function (message:String = 'Bad Request') {
+      this.status(400).json({
+        message,
+      })
+      return this
+    })
   }
 }
