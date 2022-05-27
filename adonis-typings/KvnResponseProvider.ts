06c9ff9c7555e0fb5c64632c828fbb061d0812ef
Kevin Abestilla <kev.abestilla@gmail.com>
2022-05-27 14:08:16 +0800

    Updated typings and build config

---
+++ adonis-typings/KvnResponseProvider.ts
@@ -0,0 +1,25 @@
+import TransformerAbstractI from 'adonis-bumblebee-ts/build/Bumblebee/TransformerAbstract'
+
+declare module '@ioc:Adonis/Core/Response' {
+  interface ResponseContract {
+    ok(message: String): this
+
+    data(data: {}, message: String): this
+
+    resource(resource: TransformerAbstractI, meta:{}, message: String): this
+
+    accepted(data: {}, message: String): this
+
+    error(errors: {}, message: String): this
+
+    unableToProcess(errors: {}, message: String): this
+
+    notFound(message: String): this
+
+    forbidden(message: String): this
+
+    unauthorized(message: String): this
+
+    badRequest(message: String): this
+  }
+}
